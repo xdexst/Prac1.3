d4a121176f9e0528a49ef9219b30efcc1b0e2067
xdexst <rnoy10303@gmail.com>
2024-02-20 16:42:34 +0400

    Добавление библиотек

--- main.cpp
+++ main.cpp
@@ -2,28 +2,3 @@
 #include <fstream>
 #include <string>
 using namespace std;
-
-class TBook {
-public:
-	string name;
-	string author;
-	string count;
-};
-
-int main() {
-	setlocale(LC_ALL, "ru");
-	cout << "Test v1.4: Ok.\n";
-
-	TBook book1;
-
-	book1.name = "Prikladnoe programmirovanie";
-	book1.author = "Kirill Aleksandrovi Kasatkin";
-	book1.count = "10000";
-
-	cout << "\nFirst book: \n";
-	cout << "Name: " << book1.name << endl;
-	cout << "Author: " << book1.author << endl;
-	cout << "Count: " << book1.count << endl;
-
-	return 0;
-}
